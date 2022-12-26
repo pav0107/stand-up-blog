85114a2ae9b7696d050e7767b6da4f426925895a
Pav <pav0107@gmail.com>
2022-12-26 21:26:45 +0000

    build rich text conponent

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -2,7 +2,8 @@ import { groq } from 'next-sanity';
 import Image from 'next/image';
 import { client } from '../../../../lib/sanity.client';
 import urlFor from '../../../../lib/urlFor';
-import category from '../../../../schemas/category';
+import { PortableText } from '@portabletext/react';
+import { RichTextComponents } from '../../../../components/RichTextComponents';
 
 type Props = {
   params: {
@@ -78,6 +79,8 @@ async function Post({ params: { slug } }: Props) {
           </section>
         </div>
       </section>
+
+      <PortableText value={post.body} components={RichTextComponents} />
     </article>
   );
 }
