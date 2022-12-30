f379759bc5886d70ce1f1fe8733b0c91fa0ca5ab
Pav <pav0107@gmail.com>
2022-12-30 22:02:53 +0000

    nextjs13 improvements

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -6,12 +6,14 @@ import PreviewBlogList from '../../components/PreviewBlogList';
 import BlogList from '../../components/BlogList';
 
 const query = groq`
-*[_type=='post'] {
-  ...,
-  author->,
-  categories[]->
-} | order(_createdAt desc)
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
 `;
+
+export const revalidate = 30; // revalidate this page every 30 seconds.
 
 export default async function Homepage() {
   if (previewData()) {
